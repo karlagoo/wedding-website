134d92bd49c3366a5e0d50395d73f37f65bbd50c
Karla Goo <karla_goo@yahoo.com>
2024-05-01 15:05:13 -0500

    init routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Welcome from './components/Welcome';
+import Password from './components/Signup/Password';
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <Routes>
+        <Route path='/' element={<Welcome />}>
+        <Route path='/password' element={<Password />}/>
+        </Route>
+        
+      </Routes>
+
     </div>
   );
 }
