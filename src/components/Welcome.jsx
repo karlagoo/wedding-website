e0caa6a279dea7becb9a80b6da086526be592ece
Karla Goo <karla_goo@yahoo.com>
2024-05-01 11:50:26 -0500

    Welcome page styling

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./welcome.css";
+import Pic from "../assets/Subject.png";
 
 function Welcome() {
   return (
-    <div className='container'>
-            <h1>Welcome</h1>
+    <div className="container">
+      <div className="welcome_container">
+        <div className="paragraph_container">
+          <h1>Welcome to Natalia's and Jimmy's Wedding website</h1>
+
+          <button>click me</button>
+        </div>
+
+        <img src={Pic} alt="" className="welcome_img" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Welcome
+export default Welcome;
