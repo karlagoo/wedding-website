4cce11e0c40ec4d047d02eec3a463385114e3b5e
Karla Goo <karla_goo@yahoo.com>
2024-05-02 13:49:29 -0500

    password page

--- src/components/password/Password.jsx
+++ src/components/password/Password.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import './password.css'
+
 
 function Password() {
+  const [inputValue, setInputValue] = useState('');
+
+  const submit = (e) => {
+    setInputValue(inputValue)
+    console.log(inputValue)
+  }
+  
+  const change =(e) =>{
+    setInputValue(e.target.value);
+  }
+
   return (
-    <div>
+    <div className="input_password">
       <h1>Input Password</h1>
-      <input type="text" />
-      <button>Submit</button>
+      <h3>Don't have one? Ask the bride if you're even invited lol</h3>
+      <input onChange={change} type="text" value={inputValue} />
+      <button onClick={submit}>Submit</button>
     </div>
   );
 }
