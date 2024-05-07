cd8f190296a7d7b1f83d3d21cc90e983e819b51b
Karla Goo <karla_goo@yahoo.com>
2024-05-07 12:28:52 -0500

    added img

--- src/components/captcha/Captcha.jsx
+++ src/components/captcha/Captcha.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import './captcha.css';
+import Pic1 from '../../assets/talkajimmy.png';   
 
 function Captcha() {
   return (
     <div className="grid">
-      <div className="cell">Card 1</div>
-      <div className="cell">Card 2</div>
-      <div className="cell">Card 3</div>
-      <div className="cell">Card 4</div>
-      <div className="cell">Card 5</div>
-      <div className="cell">Card 6</div>
-      <div className="cell">Card 7</div>
-      <div className="cell">Card 8</div>
-      <div className="cell">Card 9</div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
+      <div className="cell">
+        <img src={Pic1} alt="talka-jimmy" />
+      </div>
     </div>
   );
 }
