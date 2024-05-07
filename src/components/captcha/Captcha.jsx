fc739f572f658a15edc79bfdec58ab879f47e6c3
Karla Goo <karla_goo@yahoo.com>
2024-05-07 12:13:45 -0500

    captcha grid

--- src/components/captcha/Captcha.jsx
+++ src/components/captcha/Captcha.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import './captcha.css';
 
 function Captcha() {
   return (
-    <div className='captcha'>
-        
+    <div className="grid">
+      <div className="cell">Card 1</div>
+      <div className="cell">Card 2</div>
+      <div className="cell">Card 3</div>
+      <div className="cell">Card 4</div>
+      <div className="cell">Card 5</div>
+      <div className="cell">Card 6</div>
+      <div className="cell">Card 7</div>
+      <div className="cell">Card 8</div>
+      <div className="cell">Card 9</div>
     </div>
-  )
+  );
 }
 
-export default Captcha
+export default Captcha;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import "./home.css";
-import { NavLink, Link, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Captcha from "../captcha/Captcha";
 
 function Home() {
-  const navigate = useNavigate();
 
   return (
-    <header className="header">
-      <nav className="nav container">
-        <div className="menu">
-          <ul className="nav_list">
-            <li>
-              <NavLink to="/faq">FAQ</NavLink>
-            </li>
-            <li>
-              <NavLink to="/ourstory">Our Story</NavLink>
-            </li>
-            <li>
-              <NavLink to="/registry">Registry</NavLink>
-            </li>
-            <li>
-              <NavLink to="/venue">Venue</NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    <div>
+      <header className="header">
+        <nav className="nav container">
+          <div className="menu">
+            <ul className="nav_list">
+              <li>
+                <NavLink to="/faq">FAQ</NavLink>
+              </li>
+              <li>
+                <NavLink to="/ourstory">Our Story</NavLink>
+              </li>
+              <li>
+                <NavLink to="/registry">Registry</NavLink>
+              </li>
+              <li>
+                <NavLink to="/venue">Venue</NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+        <Captcha />
+    </div>
   );
 }
 
